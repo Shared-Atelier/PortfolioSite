f0ed69bebed66ff17d2433b75c18df0b5cc341d6
jan <yuta.jan.takagi@gmail.com>
2023-03-26 09:28:24 +0900

    カルーセルをレスポンシブ仕様に＋コンタクトからモーダルを削除、別ページ化

---
+++ src/resources/views/contact/contact.blade.php
@@ -0,0 +1,11 @@
+<div class='contact-form-wrapper'>
+    <div id='contact-form'>
+    <img class='close' src="{{ asset('/img/cross.png') }}" alt="">
+    <h2>Contactフォーム</h2>
+    <form action="#">
+        <input class='form-email' type="email" placeholder='メールアドレス'>
+        <textarea class='form-text' type="text" placeholder='お問合せ内容'></textarea>
+        <div id='submit-btn'>送信</div>
+    </form>
+    </div>
+</div>
