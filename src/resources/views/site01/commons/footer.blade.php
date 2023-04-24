ac26764883c165aba7d686530d473990ff5a9f34
Yuta J TAKAGI <jan@mbp.local>
2023-04-24 12:42:52 +0900

    site01 create hero

---
+++ src/resources/views/site01/commons/footer.blade.php
@@ -0,0 +1,9 @@
+<footer id='footer'>
+    <div class="container-footer">
+        <div class="container-footer-text">
+            <p>
+              ©2023 SHARED-ATELIRE, All rights reserved.
+            </p>
+        </div>
+    </div>
+</footer>
---
+++ src/resources/views/site01/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="hero_container">
+   <video autoplay loop src="{{ asset('/img/site01/Site01_hero.mp4') }}" class="hero_mov"></video> 
+</div>
+
+@endsection
