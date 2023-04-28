e5f626b6183df5bfeaf12b63b9f2713f264a920a
Yuta J TAKAGI <jan@mbp.local>
2023-04-28 17:25:14 +0900

    ヘッダーメニュー(グローバルメニュー)作成、レスポンシブ調整

--- src/resources/views/site01/commons/header.blade.php
+++ src/resources/views/site01/commons/header.blade.php
@@ -1,5 +1,5 @@
-<div class="header_container">
-    <div class="menu_btn open_btn">
+
+    <div class="open_btn">
         <span></span>
         <span></span>
         <span></span>
@@ -7,10 +7,13 @@
     <nav id="g-nav">
         <div id="g-nav-list">
             <ul>
+                <li><a href="#"><h1>トップページ</h1></a></li>
+                <li><span></span></li>
                 <li><a href="#"><h1>初めての教会</h1></a></li>
+                <li><span></span></li>
                 <li><a href="#"><h1>プロフィールと歴史</h1></a></li>
+                <li><span></span></li>
                 <li><a href="#"><h1>ミサのご案内</h1></a></li>
             </ul>
         </div>
     </nav>
-</div>
