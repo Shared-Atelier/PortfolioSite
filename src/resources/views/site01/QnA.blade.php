ad155cadf20454f8b79024d8e30263fe258cdef0
Yuta J TAKAGI <jan@yui.local>
2023-05-04 03:07:07 +0900

    全ページ一応完成

--- src/resources/views/site01/QnA.blade.php
+++ src/resources/views/site01/QnA.blade.php
@@ -15,13 +15,389 @@
     <h2>教会ご訪問に関して</h2>
     <div qna_text>
         <div class="q">
-            <img src="{{ asset('/img/site01/Question.png') }}" alt="" class="q">
-            <p>お休みの日はありますか？</p>    
+            <div class="q_img">
+                <img src="{{ asset('/img/site01/Question.png') }}" alt="" class="q">
+            </div>
+            <div class="q_text">
+                <p>お休みの日はありますか？</p>
+            </div>
         </div>
         <div class="a">
-            <img src="{{ asset('/img/site01/Answer.png') }}" alt="" class="a">
-            <p>教会の聖堂は一年中毎日開いています。神父が不在のときも、担当の係が扉の開け閉めをしています。聖堂に入れる時間は、朝7時から夕方6時までです。</p>    
+            <div class="a_img">
+                <img src="{{ asset('/img/site01/Answer.png') }}" alt="" class="a">
+            </div>
+            <div class="a_text">
+                <p>教会の聖堂は一年中毎日開いています。神父が不在のときも、担当の係が扉の開け閉めをしています。聖堂に入れる時間は、朝7時から夕方6時までです。</p>    
+            </div>
         </div>
+        <span></span>
+
+        <div class="q">
+            <div class="q_img">
+                <img src="{{ asset('/img/site01/Question.png') }}" alt="" class="q">
+            </div>
+            <div class="q_text">
+                <p>拝観料のようなものは必要ですか？</p>
+            </div>
+        </div>
+        <div class="a">
+            <div class="a_img">
+                <img src="{{ asset('/img/site01/Answer.png') }}" alt="" class="a">
+            </div>
+            <div class="a_text">
+                <p>必要ありません。教会施設に入るための料金は一切いただいていません。また当教会では、常設の献金箱も置いていません。</p>    
+            </div>
+        </div>
+        <span></span>
+
+        <div class="q">
+            <div class="q_img">
+                <img src="{{ asset('/img/site01/Question.png') }}" alt="" class="q">
+            </div>
+            <div class="q_text">
+                <p>駐車場はありますか？</p>
+            </div>
+        </div>
+        <div class="a">
+            <div class="a_img">
+                <img src="{{ asset('/img/site01/Answer.png') }}" alt="" class="a">
+            </div>
+            <div class="a_text">
+                <p>あります。敷地内に、一般車約8台と障がい者用車両2台が置けます。ただし日曜日のミサ時などはいつも満車になりますので、近隣の有料駐車場もご利用ください。</p>    
+            </div>
+        </div>
+        <span></span>
+
+        <div class="q">
+            <div class="q_img">
+                <img src="{{ asset('/img/site01/Question.png') }}" alt="" class="q">
+            </div>
+            <div class="q_text">
+                <p>私のような信仰心をもたない者が、教会の中に入ってもいいのですか？</p>
+            </div>
+        </div>
+        <div class="a">
+            <div class="a_img">
+                <img src="{{ asset('/img/site01/Answer.png') }}" alt="" class="a">
+            </div>
+            <div class="a_text">
+                <p>もちろん歓迎いたします。教会は、信者だけに限られた場所ではありません。また、教会を訪れたら神を信じなくてはいけないという約束もありません。お気軽にお入りください。</p>    
+            </div>
+        </div>
+        <span></span>
+
+        <div class="q">
+            <div class="q_img">
+                <img src="{{ asset('/img/site01/Question.png') }}" alt="" class="q">
+            </div>
+            <div class="q_text">
+                <p>私には神様の前に立つ資格などない気がしますが、受け入れてもらえますか？</p>
+            </div>
+        </div>
+        <div class="a">
+            <div class="a_img">
+                <img src="{{ asset('/img/site01/Answer.png') }}" alt="" class="a">
+            </div>
+            <div class="a_text">
+                <p>はい。教会とは、むしろ、生きる上での救いや支えを必要としている方、ご自身のことが小さく無意味なものに感じられているような方にこそ、来ていただきたいところなのです。どうぞいちど足を運んでみてください。</p>    
+            </div>
+        </div>
+        <span></span>
+
+        <div class="q">
+            <div class="q_img">
+                <img src="{{ asset('/img/site01/Question.png') }}" alt="" class="q">
+            </div>
+            <div class="q_text">
+                <p>私には神様の前に立つ資格などない気がしますが、受け入れてもらえますか？</p>
+            </div>
+        </div>
+        <div class="a">
+            <div class="a_img">
+                <img src="{{ asset('/img/site01/Answer.png') }}" alt="" class="a">
+            </div>
+            <div class="a_text">
+                <p>はい。教会とは、むしろ、生きる上での救いや支えを必要としている方、ご自身のことが小さく無意味なものに感じられているような方にこそ、来ていただきたいところなのです。どうぞいちど足を運んでみてください。</p>    
+            </div>
+        </div>
+        <span></span>
+        
+
+        
+        <br>
+        <h2>ミサについて</h2>
+        <div class="q">
+            <div class="q_img">
+                <img src="{{ asset('/img/site01/Question.png') }}" alt="" class="q">
+            </div>
+            <div class="q_text">
+                <p>ミサでは、何をしているのですか？</p>
+            </div>
+        </div>
+        <div class="a">
+            <div class="a_img">
+                <img src="{{ asset('/img/site01/Answer.png') }}" alt="" class="a">
+            </div>
+            <div class="a_text">
+                <p>聖歌を歌い、聖書の朗読を聴き、司祭の説教を味わい、みなで共に祈ります。そして、キリストによる最後の晩餐を記念する「聖体拝領」が行われます。カトリックのミサは、キリストのもとに集まった人々が、神への感謝とともにその恵みにあずかる場です。</p>    
+            </div>
+        </div>
+        <span></span>
+
+        <div class="q">
+            <div class="q_img">
+                <img src="{{ asset('/img/site01/Question.png') }}" alt="" class="q">
+            </div>
+            <div class="q_text">
+                <p>ミサを見学したいのですが、予約は必要ですか？</p>
+            </div>
+        </div>
+        <div class="a">
+            <div class="a_img">
+                <img src="{{ asset('/img/site01/Answer.png') }}" alt="" class="a">
+            </div>
+            <div class="a_text">
+                <p>予約は必要ありません。なお、あらかじめご連絡いただければ、ミサご見学の際にご案内や説明をさせていただく手配もいたします。</p>    
+            </div>
+        </div>
+        <span></span>
+
+        <div class="q">
+            <div class="q_img">
+                <img src="{{ asset('/img/site01/Question.png') }}" alt="" class="q">
+            </div>
+            <div class="q_text">
+                <p>キリスト教の信者ではありませんが、ミサに参加できますか？</p>
+            </div>
+        </div>
+        <div class="a">
+            <div class="a_img">
+                <img src="{{ asset('/img/site01/Answer.png') }}" alt="" class="a">
+            </div>
+            <div class="a_text">
+                <p>はい。ミサには、他の宗教の信者の方を含めて、どなたでもご自由に参加していただけます。「聖体拝領」はカトリックの洗礼を受けた方に限られますが、信者でない方も、司祭からの祝福を受けることができます。</p>    
+            </div>
+        </div>
+        <span></span>
+
+        <div class="q">
+            <div class="q_img">
+                <img src="{{ asset('/img/site01/Question.png') }}" alt="" class="q">
+            </div>
+            <div class="q_text">
+                <p>ミサにはどんな服装で参加したらいいですか？</p>
+            </div>
+        </div>
+        <div class="a">
+            <div class="a_img">
+                <img src="{{ asset('/img/site01/Answer.png') }}" alt="" class="a">
+            </div>
+            <div class="a_text">
+                <p>改まった服装は必要ありません。大勢の人が集まる場にふさわしい常識的なものであれば、普段着でもけっこうです。</p>    
+            </div>
+        </div>
+        <span></span>
+
+        <div class="q">
+            <div class="q_img">
+                <img src="{{ asset('/img/site01/Question.png') }}" alt="" class="q">
+            </div>
+            <div class="q_text">
+                <p>ミサに参加するのに何か持ち物は必要ですか？</p>
+            </div>
+        </div>
+        <div class="a">
+            <div class="a_img">
+                <img src="{{ asset('/img/site01/Answer.png') }}" alt="" class="a">
+            </div>
+            <div class="a_text">
+                <p>何も持たずにお越しになっても大丈夫です。聖歌の本や必要な資料は聖堂内に用意してあります。</p>    
+            </div>
+        </div>
+        <span></span>
+
+        <div class="q">
+            <div class="q_img">
+                <img src="{{ asset('/img/site01/Question.png') }}" alt="" class="q">
+            </div>
+            <div class="q_text">
+                <p>小さい子どもを連れて行ってもかまいませんか？</p>
+            </div>
+        </div>
+        <div class="a">
+            <div class="a_img">
+                <img src="{{ asset('/img/site01/Answer.png') }}" alt="" class="a">
+            </div>
+            <div class="a_text">
+                <p>大丈夫です。お子様をお預かりするようなお手伝いはできませんが、聖堂内に一緒に入っていただいてけっこうです。小さなお子たちの姿がほほえましいという声もありました。</p>    
+            </div>
+        </div>
+        <span></span>
+
+        <div class="q">
+            <div class="q_img">
+                <img src="{{ asset('/img/site01/Question.png') }}" alt="" class="q">
+            </div>
+            <div class="q_text">
+                <p>初めてなのですが、どうすればいいでしょうか？</p>
+            </div>
+        </div>
+        <div class="a">
+            <div class="a_img">
+                <img src="{{ asset('/img/site01/Answer.png') }}" alt="" class="a">
+            </div>
+            <div class="a_text">
+                <p>日曜日のミサでは聖堂入口に受付係がいますので、初めてです、とおっしゃってください。ご案内をいたします。聖堂の中では、どうぞあいている席にご自由にお座りください。<br>ミサの中で歌う聖歌や朗読聖書の言葉、ミサの次第については、会衆席手前の棚にある「典礼聖歌」等の本と、テーブル上の「聖書と典礼」・「祈りの手帖」などでご覧いただけます。<br>ご不明なことは何でも案内の係にご遠慮なくお尋ねください。</p>    
+            </div>
+        </div>
+        <span></span>
+
+        <div class="q">
+            <div class="q_img">
+                <img src="{{ asset('/img/site01/Question.png') }}" alt="" class="q">
+            </div>
+            <div class="q_text">
+                <p>ミサに参加したら献金をしなくてはいけませんか？ほんの少しでもいいですか？</p>
+            </div>
+        </div>
+        <div class="a">
+            <div class="a_img">
+                <img src="{{ asset('/img/site01/Answer.png') }}" alt="" class="a">
+            </div>
+            <div class="a_text">
+                <p>ミサの中で献金籠（かご）が会衆席に回されますが、献金は義務ではなく、神への捧げものとして、自由なこころざしのもとに行われます。入れるかどうかも、金額も、自由です。</p>    
+            </div>
+        </div>
+        <span></span>
+
+        <div class="q">
+            <div class="q_img">
+                <img src="{{ asset('/img/site01/Question.png') }}" alt="" class="q">
+            </div>
+            <div class="q_text">
+                <p>ミサに参加すると、入信をしつこく勧められないか不安ですが？</p>
+            </div>
+        </div>
+        <div class="a">
+            <div class="a_img">
+                <img src="{{ asset('/img/site01/Answer.png') }}" alt="" class="a">
+            </div>
+            <div class="a_text">
+                <p>まったくご心配には及びません。信仰を強制してはならないというのが、現代のカトリック教会の原則です。ご自身で望んでおられない方にまで入信を勧めることはありません。</p>    
+            </div>
+        </div>
+        <span></span>
+
+        <br>
+        <h2>その他、教会の活動に関して</h2>
+        <div class="q">
+            <div class="q_img">
+                <img src="{{ asset('/img/site01/Question.png') }}" alt="" class="q">
+            </div>
+            <div class="q_text">
+                <p>教会には、私と同じくらいの若い人たちもいますか？</p>
+            </div>
+        </div>
+        <div class="a">
+            <div class="a_img">
+                <img src="{{ asset('/img/site01/Answer.png') }}" alt="" class="a">
+            </div>
+            <div class="a_text">
+                <p>年配の方々から子どもまで、幅広い年齢の人たちが集まっています。若い人たちも多く、「子どもの集い」や、中学生・青年のグループ活動も盛んです。また外国出身の方もおられ国際色豊かです。</p>    
+            </div>
+        </div>
+        <span></span>
+
+        <div class="q">
+            <div class="q_img">
+                <img src="{{ asset('/img/site01/Question.png') }}" alt="" class="q">
+            </div>
+            <div class="q_text">
+                <p>聖書について教えてもらうことはできますか？</p>
+            </div>
+        </div>
+        <div class="a">
+            <div class="a_img">
+                <img src="{{ asset('/img/site01/Answer.png') }}" alt="" class="a">
+            </div>
+            <div class="a_text">
+                <p>「カトリック入門講座」・「聖書の集い」で、聖書の所々を取り上げて説明されます。また、旧約聖書を通読する「聖書百週」という会も開かれています。詳しいことは教会へお問い合わせください。</p>    
+            </div>
+        </div>
+        <span></span>
+
+        <div class="q">
+            <div class="q_img">
+                <img src="{{ asset('/img/site01/Question.png') }}" alt="" class="q">
+            </div>
+            <div class="q_text">
+                <p>神父様と話したいのですが？</p>
+            </div>
+        </div>
+        <div class="a">
+            <div class="a_img">
+                <img src="{{ asset('/img/site01/Answer.png') }}" alt="" class="a">
+            </div>
+            <div class="a_text">
+                <p>事前にお電話でご連絡いただければ、ゆっくり時間をとってお話ができます。ミサにいらっしゃれば、終わったあと立ち話程度はできますが、何かと予定もあり長い時間は難しいので、前もってお電話でのご予約をお勧めいたします。</p>    
+            </div>
+        </div>
+        <span></span>
+
+        <div class="q">
+            <div class="q_img">
+                <img src="{{ asset('/img/site01/Question.png') }}" alt="" class="q">
+            </div>
+            <div class="q_text">
+                <p>結婚式を教会で挙げたいのですが、受け付けてもらえますか？</p>
+            </div>
+        </div>
+        <div class="a">
+            <div class="a_img">
+                <img src="{{ asset('/img/site01/Answer.png') }}" alt="" class="a">
+            </div>
+            <div class="a_text">
+                <p>当教会の聖堂で挙式ができます。ただし、カトリック教会としてのいろいろな制限や条件があります。</p>    
+            </div>
+        </div>
+        <span></span>
+
+        <div class="q">
+            <div class="q_img">
+                <img src="{{ asset('/img/site01/Question.png') }}" alt="" class="q">
+            </div>
+            <div class="q_text">
+                <p>洗礼はだれでも受けられるのですか？</p>
+            </div>
+        </div>
+        <div class="a">
+            <div class="a_img">
+                <img src="{{ asset('/img/site01/Answer.png') }}" alt="" class="a">
+            </div>
+            <div class="a_text">
+                <p>はい、何も制限はありません。ただ、洗礼は「秘跡」（神の恵みにあずかる特別の機会）の一つであり、それを受ける人にとって、新しく生まれ変わるのと同じほどの重い意味をもっています。このため、通常は講座などで信仰について学んでから受けることになります。</p>    
+            </div>
+        </div>
+        <span></span>
+
+        <div class="q">
+            <div class="q_img">
+                <img src="{{ asset('/img/site01/Question.png') }}" alt="" class="q">
+            </div>
+            <div class="q_text">
+                <p>いつかカトリックの洗礼を受けたいと思いますが、どうしたらいいですか？</p>
+            </div>
+        </div>
+        <div class="a">
+            <div class="a_img">
+                <img src="{{ asset('/img/site01/Answer.png') }}" alt="" class="a">
+            </div>
+            <div class="a_text">
+                <p>まずは主任司祭（神父）にご意向をお話しください。まだ現実的なこととしてまではお考えでなくても、「カトリック入門講座」に出て、信者としての生き方がどのようなものかを知っていただくのもよいと思います。</p>    
+            </div>
+        </div>
+        <span></span>
+
 
     </div>
 </div>
--- src/resources/views/site01/choir.blade.php
+++ src/resources/views/site01/choir.blade.php
@@ -6,4 +6,179 @@
     <img src="{{ asset('/img/site01/choir2.jpeg') }}" alt="" class="choir_hero">
 </div>
 
+<div class="choir_info">
+    <h3>5月28日（聖霊降臨の主日）建設修繕積立献金</h3>
+
+    <p>＊ 9:30のミサの中で堅信式を行います。
+
+    <br>＊ ミサ中の共同祈願は天白地区担当。
+
+    <br>＊ 信者会館の清掃は千種地区担当。
+
+    <br>＜9:30のミサ終了後＞
+
+    <br>＊ 日曜学校（１～３号室）
+
+    <br>＜18:00のミサは英語ミサです＞</p>
+    <br><br>
+    <h3>May 28th (Pentecost Sunday)</h3>
+
+    <p>Donation for the reserve fund for repairs of church facilities
+
+    <br>*The Rite of Confirmation during the 9:30 Mass
+
+    <br>*Tempaku-chiku in charge of prayers for General Intercessions
+
+    <br>*Chikusa-chiku in charge of Rectory cleaning
+
+    <br>*Sunday school (Rooms 1, 2 and 3)
+
+    <br><After 9:30 Mass>
+
+    <br>*Sunday school (Rooms 1, 2 and 3)
+
+    <br><18:00 Mass in English></p>
+</div>
+
+ 
+<div class="choir_info">
+    <h3>5月21日（主の昇天）愛の献金・世界広報の日献金・建設修繕積立献金</h3>
+
+    <p>＊ ミサ中の共同祈願は名東地区担当。
+
+    <br>＊ 信者会館の清掃は東春地区担当。
+
+    <br>＜9:30のミサ終了後＞
+
+    <br>＊ 信者会館1Fホールと小聖堂、聖堂・地下ロビー・内階段掃除
+
+    <br>＊ 日曜学校（１～３号室）　　＊　宣教司牧評議会（4・5号室）
+
+    <br>＜18:00のミサは日本語ミサです＞</p>
+    <br><br>
+    <h3>May 21st (Ascension of the Lord）</h3>
+
+    <p>Donations for World Communication Day, charities and for the reserve fund for repairs of church facilities
+
+    <br>*Meito-chiku in charge of prayers for General Intercessions
+
+    <br>*Toshun-chiku in charge of Rectory cleaning
+
+    <br><After 9:30 Mass>
+
+    <br>*Clean-up: Chapel, the basement lobby, the staircase
+
+    <br>Rectory entrance hall and St. Mary Chapel
+
+    <br>*Sunday school (Rooms 1, 2 and 3)
+
+    <br>*Diocesan synod (Rooms 4 and 5)
+
+    <br><18:00 Mass in Japanese ></p>
+</div>
+ 
+<div class="choir_info">
+    <h3>5月14日（復活節第6主日） 世界広報の日献金・建設修繕積立献金</h3>
+
+    <p>＊ ミサ中の共同祈願は千種地区担当。
+
+    <br>＊ 信者会館の清掃は北地区担当。
+
+    <br>＜9:30のミサ終了後＞
+
+    <br>＊ 信者会館1Fホールと小聖堂、聖堂・地下ロビー・内階段掃除
+
+    <br>＊ 信者会館大掃除
+
+    <br>＜18:00のミサは英語ミサです＞</p>
+    <br><br>
+    <h3>May 14th (6th Easter Sunday)</h3>
+
+    <p>Donations for World Communication Day, charities and for the reserve fund for repairs of church facilities
+
+    <br>*Chikusa-chiku in charge of prayers for General Intercessions
+
+    <br>*Kita-chiku in charge of Rectory cleaning
+
+    <br><After 9:30 Mass>
+
+    <br>*Clean-up: Chapel, the basement lobby, the staircase
+
+    <br>Rectory entrance hall and St. Mary Chapel
+
+    <br>*Big cleanup of the Rectory
+
+    <br><18:00 Mass in English ></p>
+</div>
+ 
+<div class="choir_info">
+    <h3>5月7日（復活節第５主日） 一粒会の日献金・建設修繕積立献金</h3>
+
+    <p>＊ ミサ中の共同祈願は東春地区担当。
+
+    <br>＊ 信者会館の清掃は西・尾西地区担当。
+
+    <br>＊ 布池だより５月号発行。
+
+    <br>＜9:30のミサ終了後＞
+
+    <br>＊ 信者会館1Fホールと小聖堂、聖堂・地下ロビー・内階段掃除
+
+    <br>＊ 日曜学校（１～３号室）
+
+    <br>＊ 天白、東春地区（小聖堂）　　＊　中・港南地区（4号室）
+
+    <br>＊ 西・尾西地区（５号室）　　＊　東地区（和室・西）
+
+    <br>＊ 名東地区（和室・東）
+
+    <br>＊ 北、千種地区（コムニタスホール）
+
+    <br>＜18:00のミサはタガログ語ミサです＞</p>
+    <br><br>
+    <h3>May 7th (5th Easter Sunday)</h3>
+
+    <p>Donations for Ichiryu-kai and reserve fund for repairs of church facilities
+
+    <br>*Toshun-chiku in charge of prayers for General Intercessions
+
+    <br>*Nishi&Bisai-chiku in charge of Rectory cleaning
+
+    <br>*May issue of the Nunoike Catholic Church News
+
+    <br><After 9:30 Mass>
+
+    <br>*Clean-up: Chapel, the basement lobby, the staircase
+
+    <br>Rectory entrance hall and St. Mary Chapel
+
+    <br>*Sunday school (Rooms 1, 2 and 3)
+
+    <br>*Chiku-kai meetings: 
+
+    <br>Tempaku-chiku and Toshun-chiku (St. Mary Chapel)
+
+    <br>Naka&Konan-chiku (Room 4)
+
+    <br>Nishi& Bisai-chiku (Room 5)
+
+    <br>Higashi-chiku (Japanese Room west)
+
+    <br>Meito-chiku (Japanese Room east)
+
+    <br>Kita-chiku and Chikusa-chiku (Communitas Hall)
+
+    <br><18:00 Mass in Tagalog ></p>
+</div>
+ 
+<div class="choir_info">
+    <h3>5月5日（初金曜日）</h3>
+
+    <p>＊ ミサの時間 ☞ 7:00、18:30（小聖堂）</p>
+    <br><br>
+    <h3>May 5th (First Friday)</h3>
+
+    <p>*Masses at 7:00 and 18:30 (St. Mary Chapel)</p>
+</div>
+
 @endsection
